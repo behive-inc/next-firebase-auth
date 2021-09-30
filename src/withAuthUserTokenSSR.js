433f26f31a9d6db9180094a62cbe27571385d4b4
Guilherme <guiilherme.bayer@gmail.com>
2021-09-30 16:11:14 -0300

    fix: creating a unauthenticated user when certs is expired

--- src/withAuthUserTokenSSR.js
+++ src/withAuthUserTokenSSR.js
@@ -70,7 +70,13 @@ const withAuthUserTokenSSR =
         ? JSON.parse(cookieValStr)
         : {}
       if (idToken) {
-        AuthUser = await verifyIdToken(idToken, refreshToken)
+        try {
+          AuthUser = await verifyIdToken(idToken, refreshToken)
+        } catch (error) {
+          // The Key ID of the Firebase token won't correspond to a known
+          // public key, which means the token has most likely expired.
+          AuthUser = createAuthUser() // unauthenticated AuthUser
+        }
       } else {
         AuthUser = createAuthUser() // unauthenticated AuthUser
       }
